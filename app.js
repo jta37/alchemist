057fd2a4767a7278de6fb26d8684ba6484d408bf
Josh Andrews <jtandr3ws37@gmail.com>
2014-12-17 14:27:08 -0800

    install _.js, query and render tweets at /test.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // require node modules & load controllers
 var express = require ('express'),
+		request = require ('request'),
 		Twit = require ('twit'),
 		bodyParser = require ('body-parser'),
 		app = express();
@@ -12,15 +13,22 @@ var T = new Twit({
   , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 });
 
-T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
-  console.log(data);
-  console.log(typeof data);
-});
 
-T.get('statuses/user_timeline', { user_id: 'shrewd_drews', count: 100 }, function (err, data, response) {
-	console.log(data);
-});
 
+
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 1 }, function(err, data, response) {
+//   console.log(data);
+//   console.log(typeof data);
+// });
+
+// T.get('statuses/user_timeline', { user_id: 'shrewd_drews', count: 1 }, function (err, data, response) {
+// 	console.log(data.text);
+// });
+
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
 // set view engine to ejs
 app.set('view engine', 'ejs');
 
@@ -29,15 +37,46 @@ app.get("/", function(req,res) {
 	res.render('index');
 })
 
+app.get('/test', function(req, res){
+	T.get('statuses/user_timeline', { user_id: 'shrewd_drews', count: 100 }, function (err, data, response) {
+	console.log(data[0].text);
 
-app.get("/search/:searchTerm", function (req, res) {
+	var tweets = [];
 
+/// NOT ASYNCHORONOUS BAD... BAD.
+// SWAP OUT _ for async NOT YET... EVENTUALLY
+	_.each(data, function(t) {
+		tweets.push(t.text);
+	});
+	console.log(tweets);
+	res.render('test', {tweets: tweets});
 });
+})
+
 
 
 app.get("/results", function(req,res) {
 	res.render('results');
 });
+
+// Require Twitter API 
+var Twit = require ('twit');
+var _ = require('underscore');
+
+// Require AlchemyAPI
+var AlchemyAPI = require('./alchemyapi');
+var alchemyapi = new AlchemyAPI();
+
+// Test Sentiment analysis via Alchemy
+// var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
+// alchemyapi.sentiment("text", myText, {}, function(response) {
+// console.log("Sentiment: " + response["docSentiment"]["type"]);
+// });
+		
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 1 }, function(err, data, response) {
+//   //console.log(data);
+//   //console.log(typeof data);
+// });
 
 
 app.listen(3000, function (){
--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 // Require Twitter API 
 var Twit = require ('twit');
+var _ = require('underscore');
 
 // Require AlchemyAPI
 var AlchemyAPI = require('./alchemyapi');
 var alchemyapi = new AlchemyAPI();
 
-// Test Sentiment analysis via Alchemy
+//Test Sentiment analysis via Alchemy
+
 var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I can't wait to build my app!";
 alchemyapi.sentiment("text", myText, {}, function(response) {
 console.log("Sentiment: " + response["docSentiment"]["type"]);
@@ -19,11 +21,31 @@ var T = new Twit({
   , access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 });
 
-T.get('search/tweets', { q: 'banana since:2011-11-11', count: 100 }, function(err, data, response) {
-  //console.log(data);
-  //console.log(typeof data);
-});
+// Pseudocode Structure 
 
-T.get('statuses/user_timeline', { user_id: 'shrewd_drews', count: 100 }, function (err, data, response) {
-	console.log(data);
-});
+T.get('search/tweets', {q: '...', count: 100}, function (err, data, res) {
+
+	var t_results = JSON.parse(data);
+	alchemyapi.yadayada("...", ..., ..., fuction (response) {
+		console.log(" Put your output here. ");
+	})
+
+
+
+})
+
+// T.get('search/tweets', { q: 'banana since:2011-11-11', count: 1 }, function(err, data, response) {
+//   //console.log(data);
+//   //console.log(typeof data);
+// });
+
+// T.get('statuses/user_timeline', { user_id: 'shrewd_drews', count: 30 }, function (err, data, response) {
+// 	console.log(data[0].text);
+
+// 	var tweets = [];
+
+// 	_.each(data, function(t) {
+// 		tweets.push(t.text);
+// 	});
+// 	console.log(tweets);
+// });
